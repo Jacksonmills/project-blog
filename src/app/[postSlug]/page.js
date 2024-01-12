67f549d498cb20aab0f8db3344c77b69e0a2a2ae
Jacksonmills <jacksonemills@gmail.com>
2024-01-12 13:08:47 -0600

    Add metadata and dynamic rendering for blog posts (exercises 1-3)

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,21 +3,33 @@ import React from 'react';
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { loadBlogPost } from '@/helpers/file-helpers';
 
-function BlogPost() {
+const loadCachedBlogPost = React.cache((postSlug) =>
+  loadBlogPost(postSlug)
+);
+
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadCachedBlogPost(params.postSlug);
+
+  return {
+    title: `${frontmatter.title}`,
+    description: `${frontmatter.abstract.slice(0, 48)}...}`
+  };
+}
+
+async function BlogPost({ params }) {
+  const { content, frontmatter } = await loadCachedBlogPost(params.postSlug);
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={frontmatter.title}
+        publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={content} />
       </div>
     </article>
   );
